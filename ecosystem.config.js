e1ed2211fb9ced0e4fcd56af8e0901641e44f378
rhelmdev <rhelm.developer@gmail.com>
2023-01-10 19:36:13 -0600

    Very important update at ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,6 +18,7 @@ module.exports = {
                 "views",
                 "README.md",
                 "docs",
+                "database.db*"
             ],
             watch_options: {
                 followSymlinks: false,
